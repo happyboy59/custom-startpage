f192d138a4c646bf89d6975025db6e797ae1590b
Prem <premkumar.r.0509@gmail.com>
2024-03-08 23:32:58 +0530

    Modified the home pagr

--- js/clock.js
+++ js/clock.js
@@ -7,27 +7,27 @@ class Clock {
       this._el.addEventListener('click', options.toggleHelp);
       this._start();
     }
-  
+
     _setTime() {
       const date = new Date();
       let hours = $.pad(date.getHours());
       let amPm = '';
-  
+    
       if (!this._twentyFourHourClock) {
         hours = date.getHours();
         if (hours > 12) hours -= 12;
         else if (hours === 0) hours = 12;
-  
-        amPm =
-          `&nbsp;<span class="am-pm">` +
-          `${date.getHours() >= 12 ? 'PM' : 'AM'}</span>`;
+    
+        amPm = `&nbsp;<span class="am-pm">${date.getHours() >= 12 ? 'PM' : 'AM'}</span>`;
       }
-  
+    
       const minutes = $.pad(date.getMinutes());
-      this._el.innerHTML = `${hours}${this._delimiter}${minutes}${amPm}`;
+      const seconds = $.pad(date.getSeconds());
+      this._el.innerHTML = `${hours}:${minutes}:${seconds}${amPm}`;
       this._el.setAttribute('datetime', date.toTimeString());
     }
-  
+    
+
     _start() {
       this._setTime();
       setInterval(this._setTime, 1000);
